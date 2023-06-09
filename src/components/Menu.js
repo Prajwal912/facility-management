e86fc00de223a806f324c353d5388e3dbd4eaffa
prajwal1221 <prajwalshah912@gmail.com>
2023-06-09 22:55:03 +0530

    minor changes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -8,6 +8,7 @@ const Menu = () => {
         <img src="/assets/search.png" alt="" className='h-[26px]'/>
         <span className=''><img src="/assets/sort.png" alt="" className='h-[26px]' />Sort</span> 
         <span><img src="/assets/filter.png" alt="" className='h-[26px]'/>Filter</span> 
+        
     </div>
   )
 }
