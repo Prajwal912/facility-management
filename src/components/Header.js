6b34b75e1ae77a4c10efc7076021476e89aaa703
prajwal <prajwalshah912@gmail.com>
2023-06-13 14:53:10 +0530

    minor change

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,57 +1,71 @@
-import React, { useState } from 'react'
-import Accordion from './accordion'
+import React, { useState } from "react";
+import Accordion from "./accordion";
 
 const Header = () => {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   const accordionData = [
     {
-      "title": "Maintainance services",
-      "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into el"
+      title: "Maintainance services",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into el",
     },
     {
-      "title": "Work wear & Uniforms",
-      "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into el"
+      title: "Work wear & Uniforms",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into el",
     },
     {
-      "title": "Custodial services",
-      "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into el"
+      title: "Custodial services",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into el",
     },
     {
-      "title": "Facility management",
-      "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into el"
+      title: "Facility management",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into el",
     },
     {
-      "title": "Maintainance services",
-      "desc": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into el"
+      title: "Maintainance services",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into el",
     },
-  ]
+  ];
 
   return (
-    <div className='p-3'>
-      <div className="z-50 flex relative w-8 h-8 flex-col justify-between items-center" onClick={() => {
-					setOpen(!open)
-				}}>
-					{/* hamburger button */}
-					<span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "rotate-45 translate-y-3.5" : ""}`} />
-					<span className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${open ? "w-0" : "w-full"}`} />
-					<span className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "-rotate-45 -translate-y-3.5" : ""}`} />
-				</div>
-
-
-
-        <div className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out filter drop-shadow-md `}>
-          <div className='mt-8 px-5 overflow-x-hidden'>
-
-			{accordionData?.map(item => {
-        return <Accordion data={item} />
-      })}
+    <div className="p-3">
+      <div
+        className="z-50 flex relative w-8 h-8 flex-col justify-between items-center"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
+        {/* hamburger button */}
+        <span
+          className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${
+            open ? "rotate-45 translate-y-3.5" : ""
+          }`}
+        />
+        <span
+          className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${
+            open ? "w-0" : "w-full"
+          }`}
+        />
+        <span
+          className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${
+            open ? "-rotate-45 -translate-y-3.5" : ""
+          }`}
+        />
       </div>
-         
-		    </div>
 
+      <div
+        className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${
+          open ? "-translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out filter drop-shadow-md `}
+      >
+        <div className="mt-8 px-5 overflow-x-hidden">
+          {accordionData?.map((item) => {
+            return <Accordion data={item} />;
+          })}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
