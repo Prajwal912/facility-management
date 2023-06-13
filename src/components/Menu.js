651669eb86645bc1f66011f7e9c436313a8f3bc3
prajwal1221 <prajwalshah912@gmail.com>
2023-06-13 23:20:17 +0530

    menu added

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Menu = () => {
   return (
     <div className='flex justify-evenly'>
-        <p>90,000+ results</p>
-        <img src="/assets/burger.png" alt="" className='h-[28px]' />
-        <img src="/assets/search.png" alt="" className='h-[26px]'/>
-        <span className=''><img src="/assets/sort.png" alt="" className='h-[26px]' />Sort</span> 
-        <span><img src="/assets/filter.png" alt="" className='h-[26px]'/>Filter</span> 
-        
+        <p className='sm:text-[20px] text-[16px] sm:mx-[60px]'>90,000+ results</p>
+        <img src="/assets/burger.png" alt="" className='sm:h-[28px] h-[26px]' />
+        <img src="/assets/search.png" alt="" className='h-[23px] mt-[2px]'/>
+        <span className='flex sm:text-[18px] text-[16px]'><img src="/assets/sort.png" alt="" className='sm:h-[18px] h-[19px]  mt-[4px] mx-[10px] sm:mx-[20px]' />Sort</span> 
+        <span className='flex sm:text-[18px] text-[16px]'><img src="/assets/filter.png" alt="" className='sm:h-[18px] h-[16px] mt-[4px] mx-[10px] sm:mx-[20px]'/>Filter</span> 
+
     </div>
   )
 }
