b7a5d8f45c777f9fc6f018fdde8f3788811eadfc
prajwal1221 <prajwalshah912@gmail.com>
2023-06-14 10:27:37 +0530

    minor changes in body and hamburger button

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,7 +28,7 @@ const Header = ({setHideCaro}) => {
   ];
 
   return (
-    <div className="p-3">
+    <div className="p-3 w-fit">
       <div
         className="z-50 flex relative w-8 h-[1.3rem] flex-col justify-between items-center cursor-pointer"
         onClick={() => {
@@ -49,7 +49,7 @@ const Header = ({setHideCaro}) => {
           className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "hidden -rotate-45 -translate-y-3.5" : ""
             }`}
         />
-        {open && <div>
+        {open && <div className="w-fit">
           <img src="/assets/greater.png" alt="" />
           </div>}
       </div>
@@ -58,7 +58,7 @@ const Header = ({setHideCaro}) => {
         className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"
           } transition-transform duration-300 ease-in-out filter drop-shadow-md `}
       >
-        <div className="mt-12 px-5 overflow-x-hidden">
+        <div className="mt-16 px-5 overflow-x-hidden w-fit">
           {accordionData?.map((item) => {
             return <Accordion data={item} />;
           })}
