d6e0b8790a4b0dc36bce50d5f9d5bd71d99ba922
prajwal <prajwalshah912@gmail.com>
2023-06-14 15:15:45 +0530

    pagination

--- src/App.js
+++ src/App.js
@@ -1,142 +1,199 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import Accordion from './components/accordion';
-import Auctions from './components/Auctions';
-import CardOverlay from './components/Card-overlay';
-import Events from './components/Events';
-import Header from './components/Header';
-import Categories from './components/Categories';
-import Carousel from './components/Carousel';
-import { useState } from 'react';
-import Shop from './Shop';
-import Products from './Products';
-import Form from './components/Form';
-
-
-
+import Accordion from "./components/accordion";
+import Auctions from "./components/Auctions";
+import CardOverlay from "./components/Card-overlay";
+import Events from "./components/Events";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Carousel from "./components/Carousel";
+import { useState } from "react";
+import Shop from "./Shop";
+import Products from "./Products";
+import Form from "./components/Form";
 
 function App() {
-
   let auctionArr = Array.from({ length: 8 }, (value, index) => index);
   let featureArr = Array.from({ length: 9 }, (value, index) => index);
   let serviceArr = Array.from({ length: 8 }, (value, index) => index);
   let featuredArr = Array.from({ length: 3 }, (value, index) => index);
-  let overlayArr = ['Request Anything', 'Create Events', 'Auction Items'];
+  let overlayArr = ["Request Anything", "Create Events", "Auction Items"];
 
   const categoriesData = [
     {
       img: "/assets/clarity_building-line.png",
-      title: "Facility Management"
+      title: "Facility Management",
     },
     {
       img: "/assets/logistic.png",
-      title: "Logistic"
+      title: "Logistic",
     },
     {
       img: "/assets/construction.png",
-      title: "Construction"
+      title: "Construction",
     },
     {
       img: "/assets/chemicals.png",
-      title: "chemicals"
-    }
-  ]
+      title: "chemicals",
+    },
+  ];
 
-  const [hideCaro, setHideCaro] = useState(false)
-
-
+  const [hideCaro, setHideCaro] = useState(false);
+  const [activePage, setActivePage] = useState(1);
 
   return (
     <>
       <Header setHideCaro={setHideCaro} />
-      <Products />
-      <Shop />
+      {activePage === 2 && <Shop />}
+      {activePage === 3 && <Products />}
+      {activePage === 1 && (
+        <>
+          {/* overlay div */}
+          <div className="mx-4 mb-20 mt-5">
+            <div className="flex ml-[12px]">
+              {overlayArr?.map((item) => {
+                return <CardOverlay title={item} />;
+              })}
+            </div>
+          </div>
 
-      {/* overlay div */}
-      <div className='mx-4 mb-20 mt-5'>
-        <div className='flex ml-[12px]'>
-          {overlayArr?.map(item => {
-            return <CardOverlay title={item} />
-          })}
-        </div>
+          {!hideCaro && <Carousel />}
+
+          {/* categories */}
+          <div className="mx-4 mb-10 mt-5">
+            <div className="ml-2 flex justify-between mx-4 mt-3">
+              <p className="font-medium text-[22px]">
+                Explore Popular Categories
+              </p>
+              <p className="text-[#4D79EB] cursor-pointer">View all</p>
+            </div>
+            <div className="flex overflow-auto mt-5">
+              {categoriesData?.map((i) => {
+                return <Categories data={i} />;
+              })}
+            </div>
+          </div>
+
+          <div className="mx-4 mb-10 mt-5">
+            <div className="ml-2 flex justify-between mx-4 mt-3">
+              <p className="font-medium text-[22px]">Trending Auctions</p>
+              <p className="text-[#4D79EB] cursor-pointer	">View all</p>
+            </div>
+            <div className="ml-2 text-[#5F5F5F] text-[17px] mb-5">
+              See what's popular across thousands of auctions.
+            </div>
+            <div className="flex overflow-auto sm:overflow-hidden sm:flex-wrap ml-[12px]">
+              {auctionArr?.map((i) => {
+                return <Auctions />;
+              })}
+            </div>
+          </div>
+
+          <div className="mx-4 mb-10 mt-5">
+            <div className="ml-2 flex justify-between mx-4 mt-3">
+              <p className="font-medium text-[22px]">Featured Products</p>
+              <p className="text-[#4D79EB] cursor-pointer	">View all</p>
+            </div>
+            <div className="ml-2 text-[#5F5F5F] text-[17px] mb-5">
+              See what's popular across thousands of products.
+            </div>
+            <div className="flex sm:overflow-none sm:flex-wrap  overflow-auto ml-[12px]">
+              {featureArr?.map((i) => {
+                return <Auctions />;
+              })}
+            </div>
+          </div>
+
+          <div className="mx-4 mb-10 mt-5">
+            <div className="ml-2 flex justify-between mx-4 mt-3">
+              <p className="font-medium text-[22px]">Featured Services</p>
+              <p className="text-[#4D79EB] cursor-pointer">View all</p>
+            </div>
+            <div className="ml-2 text-[#5F5F5F] text-[17px] mb-5">
+              See what's popular across thousands of services.
+            </div>
+            <div className="flex overflow-auto sm:overflow-hidden sm:flex-wrap ml-[12px]">
+              {serviceArr?.map((i) => {
+                return <Auctions />;
+              })}
+            </div>
+          </div>
+
+          {/* featured events */}
+          <div className="mx-4 mb-20 mt-5">
+            <div className="ml-2 flex justify-between mx-4 mt-3">
+              <p className="font-medium text-[22px]">Featured Events</p>
+            </div>
+            <div className="ml-2 text-[#5F5F5F] text-[17px] mb-5">
+              See what's popular across thousands of Events.
+            </div>
+            <div className="flex overflow-auto ml-[12px]">
+              {featuredArr?.map((i) => {
+                return <Events />;
+              })}
+            </div>
+          </div>
+        </>
+      )}
+
+      {/* Pagination */}
+
+      <div className="flex justify-center mb-6">
+        <ul class="inline-flex items-center -space-x-px">
+          <li class="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 cursor-pointer"
+          onClick={()=> {activePage > 1 && setActivePage(activePage-1)}}
+          >
+            <span class="sr-only">Previous</span>
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </li>
+          <li class={`px-3 py-2 leading-tight ${activePage === 1 ? "text-blue-600 bg-blue-100" : "text-gray-600"} border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 cursor-pointer`}
+          onClick={() => setActivePage(1)}
+          >
+            1
+          </li>
+          <li class={`px-3 py-2 leading-tight ${activePage === 2 ? "text-blue-600 bg-blue-100" : "text-gray-600"} bg-white border border-gray-300 hover:bg-blue-100 hover:text-blue-700 cursor-pointer`}
+          onClick={() => setActivePage(2)}
+          >
+            2
+          </li>
+          <li class={`px-3 py-2 leading-tight ${activePage === 3 ? "text-blue-600 bg-blue-100" : "text-gray-600"} bg-white border border-gray-300 hover:bg-blue-100 hover:text-blue-700 cursor-pointer`}
+          onClick={() => setActivePage(3)}
+          >
+            3
+          </li>
+          <li class="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 cursor-pointer"
+          onClick={()=> {activePage < 3 && setActivePage(activePage+1)}}
+          >
+            <span class="sr-only">Next</span>
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </li>
+        </ul>
       </div>
-
-
-      {!hideCaro && <Carousel />}
-
-
-      {/* categories */}
-      <div className='mx-4 mb-10 mt-5'>
-        <div className='ml-2 flex justify-between mx-4 mt-3'>
-          <p className='font-medium text-[22px]'>Explore Popular Categories</p>
-          <p className='text-[#4D79EB] cursor-pointer'>View all</p>
-        </div>
-        <div className='flex overflow-auto mt-5'>
-          {categoriesData?.map(i => {
-            return <Categories data={i} />
-          })}
-        </div>
-      </div>
-
-
-
-      <div className='mx-4 mb-10 mt-5'>
-        <div className='ml-2 flex justify-between mx-4 mt-3'>
-          <p className='font-medium text-[22px]'>Trending Auctions</p>
-          <p className='text-[#4D79EB] cursor-pointer	'>View all</p>
-        </div>
-        <div className="ml-2 text-[#5F5F5F] text-[17px] mb-5">See what's popular across thousands of auctions.</div>
-        <div className='flex overflow-auto ml-[12px]'>
-          {auctionArr?.map(i => {
-            return <Auctions />
-          })}
-        </div>
-      </div>
-
-      <div className='mx-4 mb-10 mt-5'>
-        <div className='ml-2 flex justify-between mx-4 mt-3'>
-          <p className='font-medium text-[22px]'>Featured Products</p>
-          <p className='text-[#4D79EB] cursor-pointer	'>View all</p>
-        </div>
-        <div className="ml-2 text-[#5F5F5F] text-[17px] mb-5">See what's popular across thousands of products.</div>
-        <div className='flex sm:overflow-none sm:flex-wrap  overflow-auto ml-[12px]'>
-          {featureArr?.map(i => {
-            return <Auctions />
-          })}
-        </div>
-      </div>
-
-
-      <div className='mx-4 mb-10 mt-5'>
-        <div className='ml-2 flex justify-between mx-4 mt-3'>
-          <p className='font-medium text-[22px]'>Featured Services</p>
-          <p className='text-[#4D79EB] cursor-pointer'>View all</p>
-        </div>
-        <div className="ml-2 text-[#5F5F5F] text-[17px] mb-5">See what's popular across thousands of services.</div>
-        <div className='flex overflow-auto ml-[12px]'>
-          {serviceArr?.map(i => {
-            return <Auctions />
-          })}
-        </div>
-      </div>
-
-
-      {/* featured events */}
-      <div className='mx-4 mb-20 mt-5'>
-        <div className='ml-2 flex justify-between mx-4 mt-3'>
-          <p className='font-medium text-[22px]'>Featured Events</p>
-        </div>
-        <div className="ml-2 text-[#5F5F5F] text-[17px] mb-5">See what's popular across thousands of Events.</div>
-        <div className='flex overflow-auto ml-[12px]'>
-          {featuredArr?.map(i => {
-            return <Events />
-          })}
-        </div>
-      </div>
-
-      <Form />
-      
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -28,52 +28,67 @@ const Header = ({ setHideCaro }) => {
   ];
 
   return (
-    <div className="p-3 w-fit">
-      <div className="flex">
+    <div className="p-3">
+      <div className="flex w-full justify-between items-center">
         <div
           className="z-50 flex relative w-8 h-[1.3rem] flex-col justify-between items-center cursor-pointer"
           onClick={() => {
             setOpen(!open);
-            setHideCaro(!open)
+            setHideCaro(!open);
           }}
         >
           {/* hamburger button */}
           <span
-            className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "hidden rotate-45 translate-y-3.5" : ""
-              }`}
+            className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${
+              open ? "hidden rotate-45 translate-y-3.5" : ""
+            }`}
           />
           <span
-            className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${open ? "hidden w-0" : "w-full"
-              }`}
+            className={`h-1 w-full bg-black rounded-lg transition-all duration-300 ease-in-out ${
+              open ? "hidden w-0" : "w-full"
+            }`}
           />
           <span
-            className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${open ? "hidden -rotate-45 -translate-y-3.5" : ""
-              }`}
+            className={`h-1 w-full bg-black rounded-lg transform transition duration-300 ease-in-out ${
+              open ? "hidden -rotate-45 -translate-y-3.5" : ""
+            }`}
           />
-          {open && <div className="w-fit">
-            <img src="/assets/greater.png" alt="" />
-          </div>}
+          {open && (
+            <div className="w-fit">
+              <img src="/assets/greater.png" alt="" />
+            </div>
+          )}
         </div>
 
-        <div className="flex relative justify-between">
-        <button className={`flex px-6 py-[0.10rem] border-[#C4C4C4] bg-[#E7EDFE] rounded-[1rem] border-2 mr-2 sm:ml-3`}>
-           Products <span className="my-[0.6rem] mx-2"><img src="/assets/downarrow.png" className="h-[9px]" alt="" /></span>
-        </button>
-        <input  type="text"
-                    className="absolute left-[126px] top-[1px] z-[-1] block py-1 px-10 text-purple-700 bg-white border rounded-[1rem] focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40 w-[400px]"
-                    placeholder="Search by Products and Services"
-                />
-       <div className="absolute top-2 right-[-21rem]"><img src="/assets/search.png" alt="" /></div>
-
+        <div className="flex relative">
+          <button
+            className={`absolute flex px-3 py-[6px] border-[#C4C4C4] bg-[#E7EDFE] rounded-[1rem] border mr-2 sm:ml-3 items-center space-x-1`}
+          >
+            <span className="text-sm">Products</span>
+            <img src="/assets/vector.svg" alt="" className="w-3" />
+          </button>
+          <input
+            type="text"
+            className="ml-16 sm:ml-20 py-1 px-10 text-purple-700 bg-white border rounded-r-full  focus:outline-none focus:ring-0 w-52 sm:w-[400px]"
+            placeholder="Search by Products and Services"
+          />
+          <img
+            src="/assets/search.png"
+            alt=""
+            className="absolute right-4 top-2"
+          />
         </div>
 
-
-
+        <div className="flex items-center sm:space-x-2">
+          <img src="/assets/filter.png" alt="" />
+          <span>Filter</span>
+        </div>
       </div>
 
       <div
-        className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${open ? "-translate-x-0" : "-translate-x-full"
-          } transition-transform duration-300 ease-in-out filter drop-shadow-md `}
+        className={`absolute top-0 left-0 h-screen w-screen bg-white transform ${
+          open ? "-translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out filter drop-shadow-md `}
       >
         <div className="mt-16 px-5 overflow-x-hidden w-fit">
           {accordionData?.map((item) => {
